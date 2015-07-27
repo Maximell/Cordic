7117ede40967bc64f3720ff754968570c4f21c48
Maxim Ellison <ellisonmaxim@gmail.com>
2015-07-27 14:52:15 -0700

    added int_cordic_simp_flow.c

---
+++ cordic_implementations/int_cordic_simp_flow.c
@@ -0,0 +1,19 @@
+extern inline void int_cordic(int *x, int *y, int *z, const int *lookup) {
+	
+	int temp;
+
+	for (int i=0; i < PRECISION; i++) {
+	
+		if (*z & 0x80000000) {
+			temp = *x + (*y >> i);
+			*y = *y - (*x >> i);
+			*x = temp;
+			*z = *z + lookup[i];
+		} else {
+			temp = *x - (*y >> i);
+			*y = *y + (*x >> i);
+			*x = temp;
+			*z = *z - lookup[i];
+		}
+	}
+}
