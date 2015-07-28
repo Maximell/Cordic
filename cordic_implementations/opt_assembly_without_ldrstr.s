44bfdeff01b02b4247373f256378a955d1ff0f8d
pcatkins91 <parker.cattell.atkins@gmail.com>
2015-07-28 15:46:53 -0700

    Finished optimizing arm and cleaning up repo

--- cordic_implementations/opt_assembly_without_ldrstr.s
+++ cordic_implementations/opt_assembly_without_ldrstr.s
@@ -74,112 +74,99 @@ SCALE_FACTOR:
 	.word	1058763998
 	.text
 	.align	2
-	.global	int_cordic
-	.type	int_cordic, %function
-int_cordic:
+	.global	int_cordic_simp_flow
+	.type	int_cordic_simp_flow, %function
+	
+int_cordic_simp_flow:
 	@ Function supports interworking.
-	@ args = 0, pretend = 0, frame = 32
+	@ args = 0, pretend = 0, frame = 24
 	@ frame_needed = 1, uses_anonymous_args = 0
 	@ link register save eliminated.
 	str	fp, [sp, #-4]!
 	add	fp, sp, #0
-	sub	sp, sp, #36
-	str	r0, [fp, #-24]
-	str	r1, [fp, #-28]
-	str	r2, [fp, #-32]
-	str	r3, [fp, #-36]
+	sub	sp, sp, #28
+	
+	@store pointers to x, y, z, lookup 
+	str	r0, [fp, #-16]
+	str	r1, [fp, #-20]
+	str	r2, [fp, #-24]
+	str	r3, [fp, #-28]
+	
+	@ r0 = x, r1 = y, r2 = z and r7 = lookup (instead of loading registers from memory everytime)
+	ldr 	r0, [r0]
+	ldr 	r1, [r1]
+	ldr 	r2, [r2]
+	ldr 	r7, [r3]
+	
 	mov	r3, #0
-	str	r3, [fp, #-8]
-	b	.L2
-.L7:
-	ldr	r3, [fp, #-32]
-	ldr	r3, [r3, #0]
-	cmp	r3, #0
-	blt	.L3
-	mov	r3, #1
-	str	r3, [fp, #-16]
-	b	.L4
-.L3:
-	mov	r3, #0
-	str	r3, [fp, #-16]
-.L4:
-	ldr	r3, [fp, #-16]
-	cmp	r3, #0
-	beq	.L5
-	ldr	r3, [fp, #-24]
-	ldr	r1, [r3, #0]
-	ldr	r3, [fp, #-28]
-	ldr	r2, [r3, #0]
+	
+	@store pointer for i (for loop counter)
+	str	r3, [fp, #-8]			
+	b	.FORLOOP			@Branch to for (int i=0; i < PRECISION; i++)
+	
+.NEGATIVE_Z:
+
 	ldr	r3, [fp, #-8]
-	mov	r3, r2, asr r3
-	rsb	r3, r3, r1
-	str	r3, [fp, #-12]
-	ldr	r3, [fp, #-28]
-	ldr	r1, [r3, #0]
-	ldr	r3, [fp, #-24]
-	ldr	r2, [r3, #0]
-	ldr	r3, [fp, #-8]
-	mov	r3, r2, asr r3
-	add	r2, r1, r3
-	ldr	r3, [fp, #-28]
-	str	r2, [r3, #0]
-	ldr	r2, [fp, #-24]
+	
+	@ r5 = y >> i
+	mov r5, r1, asr r3
+	@ r6 = x >> i
+	mov r6, r0, asr r3
+	@set lookup table
+	mov r7, r3, asl #2
+	
+	@Check if z is positive/negative
+	cmp	r7, #0
+	@if positive branch to L3
+	bge	.POSITIVE_Z
+				
+	@ temp = *x + (*y >> i);
+	add	r3, r0, r5
+	str	r3, [fp, #-12]	@store result in temp
+	@ *y = *y - (*x >> i);
+	rsb	r1, r1, r6
+	@ *x = temp;
 	ldr	r3, [fp, #-12]
-	str	r3, [r2, #0]
-	ldr	r3, [fp, #-32]
-	ldr	r1, [r3, #0]
-	ldr	r3, [fp, #-8]
-	mov	r2, r3, asl #2
-	ldr	r3, [fp, #-36]
-	add	r3, r3, r2
-	ldr	r3, [r3, #0]
-	rsb	r2, r3, r1
-	ldr	r3, [fp, #-32]
-	str	r2, [r3, #0]
-	b	.L6
-.L5:
-	ldr	r3, [fp, #-24]
-	ldr	r1, [r3, #0]
-	ldr	r3, [fp, #-28]
-	ldr	r2, [r3, #0]
-	ldr	r3, [fp, #-8]
-	mov	r3, r2, asr r3
-	add	r3, r1, r3
-	str	r3, [fp, #-12]
-	ldr	r3, [fp, #-28]
-	ldr	r1, [r3, #0]
-	ldr	r3, [fp, #-24]
-	ldr	r2, [r3, #0]
-	ldr	r3, [fp, #-8]
-	mov	r3, r2, asr r3
-	rsb	r2, r3, r1
-	ldr	r3, [fp, #-28]
-	str	r2, [r3, #0]
-	ldr	r2, [fp, #-24]
+	str	r3, [r1, #0]
+	@ *z = *z + lookup[i];
+	add 	r2, r7, r2
+	
+	b	.INCREMENT
+.POSITIVE_Z:
+
+	@ temp = *x - (*y >> i);
+	rsb	r3, r0, r5
+	str	r3, [fp, #-12]	@store result in temp
+	@ *y = *y + (*x >> i);
+	add	r1, r1, r6
+	@ *x = temp;
 	ldr	r3, [fp, #-12]
-	str	r3, [r2, #0]
-	ldr	r3, [fp, #-32]
-	ldr	r1, [r3, #0]
-	ldr	r3, [fp, #-8]
-	mov	r2, r3, asl #2
-	ldr	r3, [fp, #-36]
-	add	r3, r3, r2
-	ldr	r3, [r3, #0]
-	add	r2, r1, r3
-	ldr	r3, [fp, #-32]
-	str	r2, [r3, #0]
-.L6:
+	str	r3, [r1, #0]
+	@ *z = *z - lookup[i];
+	rsb	r2, r7, r2
+
+.INCREMENT:
+	@Increment i (for loop counter)
 	ldr	r3, [fp, #-8]
 	add	r3, r3, #1
 	str	r3, [fp, #-8]
-.L2:
+.FORLOOP:
 	ldr	r3, [fp, #-8]
 	cmp	r3, #13
-	ble	.L7
+	ble	.NEGATIVE_Z
+	
+	@store final values of x, y and z
+	ldr	r3, [fp, #-16]
+	str	r0, [r3, #0]
+	ldr	r3, [fp, #-20]
+	str	r1, [r3, #0]
+	ldr	r3, [fp, #-24]
+	str	r2, [r3, #0]
+	
 	add	sp, fp, #0
 	ldmfd	sp!, {fp}
 	bx	lr
-	.size	int_cordic, .-int_cordic
+	.size	int_cordic_simp_flow, .-int_cordic_simp_flow
 	.section	.rodata
 	.align	2
 .LC0:
@@ -209,10 +196,10 @@ main:
 	mov	r0, r3
 	mov	r1, r2
 	mov	r2, ip
-	ldr	r3, .L11
-	bl	int_cordic
+	ldr	r3, .L9
+	bl	int_cordic_simp_flow
 	ldr	r3, [fp, #-16]
-	ldr	r0, .L11+4
+	ldr	r0, .L9+4
 	mov	r1, r3
 	bl	printf
 	mov	r3, #0
@@ -220,9 +207,9 @@ main:
 	sub	sp, fp, #4
 	ldmfd	sp!, {fp, lr}
 	bx	lr
-.L12:
+.L10:
 	.align	2
-.L11:
+.L9:
 	.word	LOOKUP2
 	.word	.LC0
 	.size	main, .-main
