8b96cbfa8aba2bcfcb7b9c904890cf3aa5de3fd3
Parker Atkins <parker.cattell.atkins@gmail.com>
2015-07-30 16:22:01 -0700

    Optimized assembly... I hope.

--- cordic_implementations/cordic_assembly_optimized.s
+++ cordic_implementations/cordic_assembly_optimized.s
@@ -40,6 +40,7 @@ cordic_assembly_optimized:
 	str	fp, [sp, #-4]!
 	add	fp, sp, #0
 	sub	sp, sp, #44
+	@ Store pointers for x, y, z and mode
 	str	r0, [fp, #-32]
 	str	r1, [fp, #-36]
 	str	r2, [fp, #-40]
@@ -53,15 +54,22 @@ cordic_assembly_optimized:
 	ldr	r3, [fp, #-40]
 	ldr	r3, [r3, #0]
 	str	r3, [fp, #-28]
+	@ Check mode
 	ldr	r3, [fp, #-44]
 	cmp	r3, #0
 	bne	.L2
+	@ Set z_local
 	sub	r3, fp, #28
 	str	r3, [fp, #-20]
 	b	.L3
+@ Set y_local
 .L2:
 	sub	r3, fp, #24
 	str	r3, [fp, #-20]
+
+@ x_local = x_local << 16;
+@ y_local = y_local << 16;
+@ z_local = z_local << 16;
 .L3:
 	ldr	r3, [fp, #-12]
 	mov	r3, r3, asl #16
@@ -93,49 +101,53 @@ cordic_assembly_optimized:
 	ldr	r3, [fp, #-44]
 	cmp	r3, #0
 	beq	.L7
-.L6:
+@ Rotation mode with negative value
+.L6:	
+	ldr	r4, [fp, #-12]
+	ldr	r5, [fp, #-24]
+	ldr	r6, [fp, #-28]
+	ldr 	r7, [fp, #-16]
+	ldr	r8, [fp, #-8]
 	ldr	r2, [fp, #-24]
-	ldr	r3, [fp, #-8]
-	mov	r2, r2, asr r3
-	ldr	r3, [fp, #-12]
-	add	r3, r3, r2
-	str	r3, [fp, #-12]
-	ldr	r1, [fp, #-24]
-	ldr	r2, [fp, #-16]
-	ldr	r3, [fp, #-8]
-	mov	r3, r2, asr r3
-	rsb	r3, r3, r1
-	str	r3, [fp, #-24]
-	ldr	r2, [fp, #-8]
+	mov 	r2, r2, asr r8
+	add 	r4, r4, r2
+	mov	r7, r7, asr r8
+	rsb	r5, r7, r5		
 	ldr	r3, .L11
-	ldr	r2, [r3, r2, asl #2]
-	ldr	r3, [fp, #-28]
-	add	r3, r2, r3
-	str	r3, [fp, #-28]
+	ldr	r2, [r3, r8, asl #2]
+	add	r6, r2, r6
+	str	r4, [fp, #-12]
+	str	r5, [fp, #-24]
+	str	r6, [fp, #-28]
 	b	.L8
+@ Rotation mode with positive value OR Vectoring mode
 .L7:
-	ldr	r2, [fp, #-24]
-	ldr	r3, [fp, #-8]
-	mov	r2, r2, asr r3
-	ldr	r3, [fp, #-12]
-	rsb	r3, r2, r3
-	str	r3, [fp, #-12]
-	ldr	r2, [fp, #-16]
-	ldr	r3, [fp, #-8]
-	mov	r2, r2, asr r3
-	ldr	r3, [fp, #-24]
-	add	r3, r2, r3
-	str	r3, [fp, #-24]
+
+	ldr	r4, [fp, #-12]
+	ldr	r5, [fp, #-24]
+	ldr	r6, [fp, #-28]
+	ldr 	r7, [fp, #-16]
+	ldr	r8, [fp, #-8]
+	ldr 	r2, [fp, #-24]
+	mov	r2, r2, asr r8
+	rsb	r4, r2, r4
+	mov	r7, r7, asr r8
+	add	r5, r7, r5
 	ldr	r1, [fp, #-28]
 	ldr	r2, [fp, #-8]
 	ldr	r3, .L11
-	ldr	r3, [r3, r2, asl #2]
-	rsb	r3, r3, r1
-	str	r3, [fp, #-28]
+	ldr	r3, [r3, r8, asl #2]
+	rsb	r6, r3, r6
+	str	r4, [fp, #-12]
+	str	r5, [fp, #-24]
+	str	r6, [fp, #-28]
+@ Increment loop counter
 .L8:
 	ldr	r3, [fp, #-8]
 	add	r3, r3, #1
 	str	r3, [fp, #-8]
+
+@ Forloop : for (int i = 0; i < 14; i++)
 .L4:
 	ldr	r3, [fp, #-8]
 	cmp	r3, #13
