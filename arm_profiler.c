ef9b5ca5f989490d32206b7598c054c81792a10f
Maxim Ellison <ellisonmaxim@gmail.com>
2015-07-30 11:07:20 -0700

    added arm profiler.

---
+++ arm_profiler.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+// imports for time
+#include <sys/times.h>
+#include <unistd.h>
+
+#include "define.h"
+
+// c cordic implementations
+#include "cordic_implementations/cordic.c"
+
+// arm
+#include "cordic_implementations/cordic_assembly.h"
+
+clock_t get_time() {
+	struct tms time_struct;
+	times(&time_struct);
+	return time_struct.tms_utime;
+}
+
+void check_32_bit() {
+	if( ! (!(0 & 0x80000000)
+            && !(1 & 0x80000000)
+            && !(100 & 0x80000000)
+            && !!(-1 & 0x80000000)
+            && !!(-100 & 0x80000000))) {
+        printf("Your platform doesn't support 32 bit integers. You must use a platform that supports 32 bit integers.\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(void) {
+
+	clock_t before_time;
+	int ticks_per_second = sysconf(_SC_CLK_TCK);
+
+	int repetitions = 1000000;
+
+	volatile int i;
+	int values[repetitions];
+	int angles[repetitions];
+	for (i = 0; i < repetitions; i++) {
+		values[i] = rand() % 10000;
+		angles[i] = rand() % HALF_PI;
+	}
+
+	int x, y, z;
+
+	check_32_bit();
+
+	// int_cordic
+	before_time = get_time();
+	for (i=0; i<repetitions; i++) {
+		x = values[i];
+		y = angles[repetitions-1-i];
+		z = angles[i];
+		cordic(&x, &y, &z, ROTATIONAL);
+	}
+	printf("\tcordic\ntime: %f\n\n", (double)(get_time() - before_time) / ticks_per_second);
+
+
+	// arm implementation
+	before_time = get_time();
+	for (i=0; i<repetitions; i++) {
+		x = values[i];
+		y = angles[repetitions-1-i];
+		z = angles[i];
+		cordic_assembly(&x, &y, &z, ROTATIONAL);
+	}
+	printf("\tcordic\ntime: %f\n\n", (double)(get_time() - before_time) / ticks_per_second);
+
+	return 0;
+}
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 all: calc_validator profiler
-arm: arm_validator
+arm: arm_validator arm_profiler
 
 calc_validator:
 	gcc -Wall -std=c99 -o build/calc_validator calc_validator.c
@@ -9,6 +9,9 @@ profiler:
 
 arm_validator:
 	arm-linux-gcc -Wall --static -std=c99 -o arm_build/arm_calc_validator arm_calc_validator.c cordic_implementations/cordic_assembly.s
+
+arm_profiler:
+	arm-linux-gcc -Wall --static -std=c99 -o arm_build/arm_profiler arm_profiler.c cordic_implementations/cordic_assembly.s
 
 cordic_float:
 	gcc -o Cordic_Float cordic_float.c -lm
